fe1689a9c4f1ca2b758461e9209d568330db1689
Luqman Siddiqui <luqman.siddiqui154@gmail.com>
2025-11-14 22:11:25 -0600

    Create AprilTagDistanceV3.java

---
+++ AprilTagDistanceV3.java
@@ -0,0 +1,120 @@
+
+package org.firstinspires.ftc.teamcode.bakar;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+
+@TeleOp(name = "Concept: AprilTag Localization", group = "Concept")
+
+public class MyConceptAprilTagLocalization extends LinearOpMode {
+
+    private static final boolean USE_WEBCAM = true; 
+    private Position cameraPosition = new Position(DistanceUnit.INCH,
+            0, 0, 0, 0);
+    private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
+            0, -90, 0, 0);
+
+    private AprilTagProcessor aprilTag;
+    private VisionPortal visionPortal;
+
+    DcMotor outtakeR;
+    DcMotor outtakeL;
+    DcMotor intake;
+    
+    @Override
+    public void runOpMode() {
+
+        initAprilTag();
+        
+        outtakeL = hardwareMap.get(DcMotor.class, "outtakeL");
+        outtakeR = hardwareMap.get(DcMotor.class, "outtakeR");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch START to start OpMode");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            telemetryAprilTag();
+
+            telemetry.update();
+            
+            if (gamepad1.dpad_down) {
+                visionPortal.stopStreaming();
+            } else if (gamepad1.dpad_up) {
+                visionPortal.resumeStreaming();
+            }
+            sleep(20);
+        }
+        visionPortal.close();
+    }  
+    private void initAprilTag() {
+        aprilTag = new AprilTagProcessor.Builder()
+                .setCameraPose(cameraPosition, cameraOrientation)
+                .build();
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+        builder.addProcessor(aprilTag);
+
+        visionPortal = builder.build();
+    }  
+    private void telemetryAprilTag() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                if (!detection.metadata.name.contains("Obelisk")) {
+                    telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)",
+                            detection.robotPose.getPosition().x,
+                            detection.robotPose.getPosition().y,
+                            detection.robotPose.getPosition().z));
+                    telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)",
+                            detection.robotPose.getOrientation().getPitch(AngleUnit.DEGREES),
+                            detection.robotPose.getOrientation().getRoll(AngleUnit.DEGREES),
+                            detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES)));
+                    
+                    double distance = detection.ftcPose.range;
+                    telemetry.addLine(String.format("Distance to Tag: %6.1f inches", distance));
+                }
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+                
+                double distance = detection.ftcPose.range;
+                double power = distance * 0.1;
+                outtakeR.setPower(power);
+                outtakeL.setPower(-power);
+                intake.setPower(1);
+                
+                telemetry.addLine(String.format("Distance to Tag: %6.1f inches", distance));
+            }
+        }  
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+    }  
+}
