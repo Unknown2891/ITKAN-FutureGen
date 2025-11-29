6706036b156c5c9d9782d3ff68c85044567f53d7
Luqman Siddiqui <luqman.siddiqui154@gmail.com>
2025-11-29 11:48:45 -0600

    Create year2 fieldcenteric telyop V1.java
    it works!

---
+++ year2 fieldcenteric telyop V1.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
+public class Field extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+    
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("fl");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("bl");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("fr");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("br");
+        
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double x = gamepad1.left_stick_y;
+            double y = gamepad1.left_stick_x;
+            double rx = -gamepad1.right_stick_x;
+
+            if (gamepad1.a) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1; 
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY - rotX - rx) / denominator;
+            double backLeftPower = (rotY + rotX - rx) / denominator;
+            double frontRightPower = (rotY + rotX + rx) / denominator;
+            double backRightPower = (rotY - rotX + rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+        }
+    }
+}
